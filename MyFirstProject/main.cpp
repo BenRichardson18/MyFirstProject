8f7550047222a2f00f6f172066b1986b9fec1c9c
BenRichardson18 <richardson139@tritons.iowacentral.edu>
2022-08-25 16:28:49 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,13 @@
+/*
+Ben Richardson
+C++ Fall 2022
+Due: August 31st
+Lab 1 Introduction to C++ Programming
+Demonstration of my first lines of code in C++
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!"; 
+	return 0; 
+}
